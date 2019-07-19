cdf988d1cde5b8f3cbbae04f01781e521a0c1e14
vasyavin <vasyavin17@gmail.com>
2019-07-19 15:59:32 +0300

    lesson 63

---
+++ src/api/api.js
@@ -0,0 +1,24 @@
+import * as axios from "axios";
+
+
+const instance = axios.create({
+
+    withCredentials: true,
+    baseURL:`https://social-network.samuraijs.com/api/1.0/`,
+    headers: {
+        "API-KEY": "cc893330-5839-4b82-8f5d-4b6813bea0c7"
+    }
+
+});
+
+export const usersAPI = {
+    getUsers (currentPage = 1, pageSize = 10)
+{
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`,
+    )
+        .then(response => {
+            return response.data;
+        })
+
+}
+}
