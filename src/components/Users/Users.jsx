9ed1cfcd4bbaa4773922163c8bfd86118b47f7ca
vasyavin <vasyavin17@gmail.com>
2019-07-20 10:16:50 +0300

    lesson 64

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -32,8 +32,8 @@ let Users = (props) => {
                     </div>
                     <div>
                         {u.followed ?
-                            <button onClick={() => {
-
+                            <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
+                                props.toggleFollowingProgress(true, u.id);
                                 axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
                                     withCredentials: true,
                                     headers: {
@@ -44,11 +44,12 @@ let Users = (props) => {
                                         if(response.data.resultCode == 0){
                                             props.unfollow(u.id)
                                         }
+                                        props.toggleFollowingProgress(false, u.id);
                                     });
 
                             }}>UNFOLLOW</button>
-                            : <button onClick={() => {
-
+                            : <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
+                                props.toggleFollowingProgress(true, u.id);
                                 axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
                                     withCredentials: true,
                                     headers: {
@@ -61,6 +62,7 @@ let Users = (props) => {
                                         if(response.data.resultCode == 0){
                                             props.follow(u.id)
                                         }
+                                        props.toggleFollowingProgress(false, u.id);
                                     });
 
                             }}>FOLLOW</button>}
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -6,7 +6,7 @@ import {
     setUsers,
     setTotalUsersCount,
     toggleIsFetching,
-    unfollow
+    unfollow, toggleFollowingProgress
 } from '../../redux/users-reducer';
 import * as axios from "axios";
 import Users from './Users';
@@ -45,6 +45,8 @@ class UsersContainer extends React.Component {
                     users={this.props.users}
                     follow={this.props.follow}
                     unfollow={this.props.unfollow}
+                    toggleFollowingProgress={this.props.toggleFollowingProgress}
+                    followingInProgress={this.props.followingInProgress}
             />
         </>
     }
@@ -57,7 +59,8 @@ let mapStateToProps = (state) => {
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUsersCount,
         currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching
+        isFetching: state.usersPage.isFetching,
+        followingInProgress: state.usersPage.followingInProgress
     }
 }
 /*
@@ -90,5 +93,5 @@ let mapDispatchToProps = (dispatch) => {
 
 
 export default connect(mapStateToProps, {
-    follow, unfollow, setUsers, setCurrentPage,  setTotalUsersCount,  toggleIsFetching
+    follow, unfollow, setUsers, setCurrentPage,  setTotalUsersCount,  toggleIsFetching, toggleFollowingProgress
     })(UsersContainer);
